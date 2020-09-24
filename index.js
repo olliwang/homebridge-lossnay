35bda67defe7c67f191ae5cde23127d7710da32f
Olli Wang <olliwang@ollix.com>
2020-09-24 17:23:59 +0800

    First version.

---
+++ index.js
@@ -0,0 +1,101 @@
+const i2c = require('i2c-bus')
+const i2c1 = i2c.openSync(1)
+
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Characteristic = homebridge.hap.Characteristic;
+  Service = homebridge.hap.Service;
+
+  homebridge.registerAccessory("homebridge-lossnay",
+                               "Lossnay ",
+                               Lossnay);
+}
+
+function Lossnay(log, config) {
+  function boolValueWithDefault(value, defaultValue) {
+    if (value == undefined) {
+      return defaultValue;
+    } else {
+      return value;
+    }
+  }
+
+  this.debug = boolValueWithDefault(config.debug, false);
+  this.name = config.name || 'Lossnay';
+  this.displayName = config.name;
+
+  this.services = [];
+
+  this.fanSpeed = 1;
+  this.setFanSpeed(this.fanSpeed);
+}
+
+Lossnay.prototype = {
+  setFanSpeed: function(speed) {
+    this.fanSpeed = speed;
+    const DEVICE_ADDRESS = 0x62
+    const CMD_WRITEDAC = 0x40
+    const CMD_WRITEDACEEPROM = 0x60
+
+    var value = 0;
+    if (speed > 0) {
+      const voltage = speed + 1;
+      value = voltage / 5.0 * 4095;
+      if (value > 4095) {
+        value = 4095
+      }
+    }
+
+    new Promise((resolve, reject) => {
+      i2c1.writeI2cBlock(
+          DEVICE_ADDRESS, CMD_WRITEDAC, 2,
+          Buffer.from([(value >> 4) & 0xFF, (value << 4) & 0xFF]),
+          (err, bufferWritten, buffer) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(bufferWritten, buffer);
+          });
+    });
+  },
+
+  getServices: function() {
+    var services = [];
+
+    var infoService = new Service.AccessoryInformation();
+    infoService
+        .setCharacteristic(Characteristic.Manufacturer, "Mitsubishi")
+        .setCharacteristic(Characteristic.Model, "Lossnay")
+    services.push(infoService);
+
+    var fanService = new Service.Fanv2(this.name);
+
+    fanService
+      .getCharacteristic(Characteristic.Active)
+      .on('get', function(callback) {
+        callback(null, true);
+      }.bind(this))
+      .on('set', function(value, callback) {
+        callback(null);
+      }.bind(this));
+
+    fanService
+        .addCharacteristic(Characteristic.RotationSpeed)
+        .setProps({
+          minValue: 0,
+          maxValue: 4,
+          minStep: 1,
+        })
+        .on('get', function(callback) {
+          callback(null, this.fanSpeed);
+        }.bind(this))
+        .on('set', function(value, callback) {
+          callback(null);
+        }.bind(this));
+
+      services.push(fanService);
+
+      return services;
+  }
+}
